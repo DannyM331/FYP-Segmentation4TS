fd2ef0e424f6fb0441f0021030fb915e0460120b
DannyM331 <71894620+DannyM331@users.noreply.github.com>
2024-03-21 15:41:14 +0000

    Data has been incorrect, binseg working with new data, trying floss

--- Functions/evaluate_floss.py
+++ Functions/evaluate_floss.py
@@ -2,12 +2,11 @@ import numpy as np
 from Functions.metrics import f_measure, covering
 from Algorithms.floss import floss
 
-# In evaluate_floss.py
 def evaluate_floss(dataset, routine, subject, sensor, sample_rate, cps, activities, ts, **seg_kwargs):
     profile, found_cps = floss(ts, 20*sample_rate, sample_rate, n_cps=len(cps), return_cac=True, **seg_kwargs)
 
     f1_score = f_measure({0: cps}, found_cps, margin=int(ts.shape[0] * .01))
     covering_score = covering({0: cps}, found_cps, ts.shape[0])
 
-    # Return these values without converting to list since they are already lists
+    # print(f"{dataset}: F1-Score: {np.round(f1_score, 3)}, Covering-Score: {np.round(covering_score, 3)}")
     return dataset, cps, found_cps, np.round(f1_score, 3), np.round(covering_score, 3), profile
